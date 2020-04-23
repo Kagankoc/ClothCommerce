89f7c23c541ea0a7a4eb5bbc767d4f238feaf0f2
Kağan Koç <cemsukagan@hotmail.com>
2020-04-23 23:43:43 +0300

    updated router

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuITem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuITem = ({ title, imageUrl, size, history ,linkUrl,match}) => (
+  <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
     <div
       className="background-image"
       style={{
@@ -16,4 +17,4 @@ const MenuITem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuITem;
+export default withRouter(MenuITem);
