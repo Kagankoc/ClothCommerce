9b6d18188b0d408d68b7fe0daa56a1c562605fed
Kağan Koç <cemsukagan@hotmail.com>
2020-04-27 16:02:18 +0300

    added shop page styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 
@@ -10,6 +13,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div className="App">
+    <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path='/shop' component={ShopPage}/>
